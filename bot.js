ff150de0a1509e4a5d0760282fb5e726d645d469
soondjnf <40675551+soondjnf@users.noreply.github.com>
2018-07-28 12:08:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,1 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-let timer;
-console.log("BOT ONLINE");
-
-client.on("guildMemberAdd", member => {
-    timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
-  let words = [`*** سلام عليكم يا عيني ممكن تنورنا ناقصنا عسل بخطفك *** :heart:
-                               [ https://discord.gg/d836634 ]  `]
-  setTimeout(() =>{
-  member.createDM().then(function (channel) {
-  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`).then(() => {
-})}).catch(console.error)
-}, timer)
-})
-
-client.login(process.env.BOT_TOKEN);
+كس أمكم
